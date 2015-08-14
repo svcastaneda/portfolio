cd61df500e6c9779ebaa6390dcc5a9b9bda57053
Saundra Castaneda <svcastaneda@gmail.com>
2015-08-14 00:41:03 -0500

    Portfolio with resume and admin feature, first commit

---
+++ resume/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Accomplishment(models.Model):
+    author = models.ForeignKey('auth.User')
+    text = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
