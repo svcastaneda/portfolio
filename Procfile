728d2d6b999c8d2c83496718a297a29cb87130c4
Saundra Castaneda <svcastaneda@gmail.com>
2015-08-19 21:26:18 -0500

    Attempt at deploying with Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mysite.app --log-file -
--- mysite/settings.py
+++ mysite/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -102,3 +105,24 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
