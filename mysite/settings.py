37b544dfabe34b3d9d671045c1063ff0472252b0
svcastaneda <svcastaneda@gmail.com>
2015-12-10 14:49:11 -0600

    Fix settings file type

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
  }
 
 # Parse database configuration from $DATABASE_URL
